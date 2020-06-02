745d28f7be110af54b0b5be5c33403c5704d592d
Tom Peters <weters@me.com>
2020-06-01 20:18:24 -0400

    More animations. Show pot and cards left

--- src/components/games/passthepoop/PassThePoop.vue
+++ src/components/games/passthepoop/PassThePoop.vue
@@ -1,15 +1,30 @@
 <template>
     <div class="pass-the-poop">
         <h3>Pass the Poop / {{ gameData.gameState.edition }} Edition</h3>
+
         <pass-the-poop-participants :participants="participants"/>
 
         <player-bar :error="error" :is-turn="isTurn">
             <div class="bar">
                 <div class="card">
                     <transition name="card" mode="out-in">
-                        <playing-card :rank="card.rank" :suit="card.suit" v-if="card" :key="`${card.rank}.${card.suit}`"/>
+                        <playing-card :rank="card.rank" :suit="card.suit" v-if="card"
+                                      :key="`${card.rank}.${card.suit}`"/>
                     </transition>
                 </div>
+
+                <div class="metadata">
+                    <div class="item">
+                        <span class="icon"><mdi-icon :icon="mdiPokerChip"/></span>
+                        <span class="value">{{ formatAmount(pot) }}</span>
+                    </div>
+
+                    <div class="item">
+                        <span class="icon"><mdi-icon :icon="mdiCards"/></span>
+                        <span class="value">{{ cardsLeftInDeck }}</span>
+                    </div>
+                </div>
+
                 <div class="buttons">
                     <button
                             type="button"
@@ -36,16 +51,24 @@
     import PlayingCard from "../../PlayingCard"
     import PassThePoopParticipants from "./PassThePoopParticipants"
     import showError from "../../../mixins/show_error"
+    import {tween} from 'popmotion'
+    import MdiIcon from "../../MdiIcon"
+    import {mdiCards, mdiPokerChip} from "@mdi/js"
+    import balance from "../../../mixins/balance"
 
     export default {
         name: "PassThePoop",
-        components: {PassThePoopParticipants, PlayingCard, PlayerBar},
-        mixins: [showError],
+        components: {MdiIcon, PassThePoopParticipants, PlayingCard, PlayerBar},
+        mixins: [showError, balance],
         data() {
             return {
+                mdiCards,
+                mdiPokerChip,
                 error: null,
                 confirmStay: false,
                 confirmTrade: false,
+                cardsLeftInDeck: 52,
+                pot: 0,
             }
         },
         computed: {
@@ -72,10 +95,50 @@
                     .catch(err => this.showError(err))
             },
         },
+        watch: {
+            'gameData.gameState.participants': {
+                handler: function (newValue) {
+                    const totalLives = this.gameData.gameState.lives
+                    const ante = this.gameData.gameState.ante
+                    let pot = 0
+                    newValue.forEach(p => pot += (totalLives - p.lives) * ante / totalLives)
+                    const oldPot = this.pot
+
+                    if (pot !== oldPot) {
+                        setTimeout(() => {
+                            tween({
+                                from: oldPot,
+                                to: pot,
+                                duration: 500,
+                            }).pipe(Math.round).start(val => {
+                                this.pot = val
+                            })
+                        }, 1000)
+                    }
+                },
+                immediate: true,
+            },
+            'gameData.gameState.cardsLeftInDeck': {
+                handler: function (newValue, oldValue) {
+                    // doing it this way allows us to animate from 52
+                    this.cardsLeftInDeck = newValue
+                    tween({
+                        from: parseInt(oldValue || 52, 10),
+                        to: parseInt(newValue, 10),
+                        duration: 500,
+                    }).pipe(Math.round).start(val => {
+                        this.cardsLeftInDeck = val
+                    })
+                },
+                immediate: true,
+            },
+        },
     }
 </script>
 
 <style lang="scss" scoped>
+    @import '../../../variables';
+
     .pass-the-poop {
 
         .card {
@@ -100,7 +163,28 @@
     }
 
     .card-enter, .card-leave-to {
-        opacity: 0;
+        opacity:   0;
         transform: translateY(-100%);
+    }
+
+    .metadata {
+        display:     flex;
+        margin-left: $spacing;
+
+        .item {
+            display:        flex;
+            flex-direction: column;
+            align-items:    center;
+            width:          min-content;
+
+            &:not(:first-child) {
+                margin-left: $spacing-small;
+            }
+
+            span.icon {
+                display: block;
+                width:   30px;
+            }
+        }
     }
 </style>
