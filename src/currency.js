450977cf33c040ac23894ad96b95c0f61479a4c5
Tom Peters <weters@me.com>
2021-02-14 22:05:18 -0500

    Fix -0

--- src/currency.js
+++ src/currency.js
@@ -4,6 +4,10 @@ const formatter = new Intl.NumberFormat('en-US', {
 })
 
 export const formatAmount = (cents) => {
+    if (cents === 0) {
+        cents = 0 // make sure we get rid of -0
+    }
+
     const dollars = cents / 100
     return formatter.format(dollars).replace(/.00$/, '')
 }
