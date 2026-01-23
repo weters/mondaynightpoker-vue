2495434d5e44babfc348fe9639b06b6c2aef6f67
Tom Peters <weters@me.com>
2026-01-22 22:16:34 -0500

    Fix pass the poop

--- src/components/PlayingCard.vue
+++ src/components/PlayingCard.vue
@@ -8,204 +8,204 @@
             <span class="center">
                 <span class="suit"><mdi-icon :icon="displaySuit"/></span>
             </span>
-            <mdi-icon class="wild" :icon="mdiStarCircleOutline" v-if="isWild" />
+            <mdi-icon class="wild" :icon="mdiStarCircleOutline" v-if="isWild"/>
         </div>
     </div>
 </template>
 
 <script>
 import {mdiCardsClub, mdiCardsDiamond, mdiCardsHeart, mdiCardsSpade, mdiStar, mdiStarCircleOutline} from '@mdi/js'
-    import MdiIcon from "@/components/MdiIcon.vue"
-    import {mapGetters} from "vuex"
+import MdiIcon from "@/components/MdiIcon.vue"
+import {mapGetters} from "vuex"
 
-    export default {
-        name: "PlayingCard",
-        components: {MdiIcon},
-        props: {
-            suit: {
-                type: String,
-                required: true,
-            },
-            rank: {
-                type: Number,
-                required: true,
-            },
-            isWild: {
-                type: Boolean,
-                default: false,
+export default {
+    name: "PlayingCard",
+    components: {MdiIcon},
+    props: {
+        suit: {
+            type: String,
+            required: true,
+        },
+        rank: {
+            type: Number,
+            required: true,
+        },
+        isWild: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    data() {
+        return {
+            mdiCardsClub,
+            mdiCardsDiamond,
+            mdiCardsHeart,
+            mdiCardsSpade,
+            mdiStarCircleOutline,
+            mdiStar,
+        }
+    },
+    computed: {
+        ...mapGetters({
+            isTurn: 'bourre/isTurn',
+        }),
+        classes() {
+            const classes = {
+                'big-card': true,
             }
+            classes[this.suit] = true
+            return classes
         },
-        data() {
-            return {
-                mdiCardsClub,
-                mdiCardsDiamond,
-                mdiCardsHeart,
-                mdiCardsSpade,
-                mdiStarCircleOutline,
-                mdiStar,
+        displayRank() {
+            switch (this.rank) {
+                case 11:
+                    return 'J'
+                case 12:
+                    return 'Q'
+                case 13:
+                    return 'K'
+                case 14:
+                    return 'A'
             }
-        },
-        computed: {
-            ...mapGetters({
-                isTurn: 'bourre/isTurn',
-            }),
-            classes() {
-                const classes = {
-                    'big-card': true,
-                }
-                classes[this.suit] = true
-                return classes
-            },
-            displayRank() {
-                switch (this.rank) {
-                    case 11:
-                        return 'J'
-                    case 12:
-                        return 'Q'
-                    case 13:
-                        return 'K'
-                    case 14:
-                        return 'A'
-                }
 
-                return this.rank
-            },
-            displaySuit() {
-                switch (this.suit) {
-                    case 'clubs':
-                        return mdiCardsClub
-                    case 'diamonds':
-                        return mdiCardsDiamond
-                    case 'hearts':
-                        return mdiCardsHeart
-                    case 'spades':
-                        return mdiCardsSpade
-                    case 'stars':
-                        return mdiStar
-                }
-
-                throw new Error('unknown suit')
-            },
-            formattedRank() {
-                switch (this.rank) {
-                    case 11:
-                        return 'J'
-                    case 12:
-                        return 'Q'
-                    case 13:
-                        return 'K'
-                    case 14:
-                        return 'A'
-                }
-
-                return this.rank
-            },
-            card() {
-                return `${this.formattedRank} of ${this.suit}`
-            },
+            return this.rank
         },
-        methods: {
-            sizeCard() {
-                const size = this.$el.clientHeight / 4
-                this.$refs.cornerRank.style.fontSize = `${size}px`
-                this.$refs.cornerRank.style.lineHeight = `${size}px`
-                this.$refs.cornerSuit.style.width = `${size}px`
-                this.$refs.cornerSuit.style.height = `${size}px`
-            },
-        },
-        watch: {
-            isTurn() {
-                this.sizeCard()
+        displaySuit() {
+            switch (this.suit) {
+                case 'clubs':
+                    return mdiCardsClub
+                case 'diamonds':
+                    return mdiCardsDiamond
+                case 'hearts':
+                    return mdiCardsHeart
+                case 'spades':
+                    return mdiCardsSpade
+                case 'stars':
+                    return mdiStar
             }
+
+            throw new Error('unknown suit')
         },
-        mounted() {
-            window.addEventListener('resize', this.sizeCard)
+        formattedRank() {
+            switch (this.rank) {
+                case 11:
+                    return 'J'
+                case 12:
+                    return 'Q'
+                case 13:
+                    return 'K'
+                case 14:
+                    return 'A'
+            }
+
+            return this.rank
+        },
+        card() {
+            return `${this.formattedRank} of ${this.suit}`
+        },
+    },
+    methods: {
+        sizeCard() {
+            const size = this.$el.clientHeight / 4
+            this.$refs.cornerRank.style.fontSize = `${size}px`
+            this.$refs.cornerRank.style.lineHeight = `${size}px`
+            this.$refs.cornerSuit.style.width = `${size}px`
+            this.$refs.cornerSuit.style.height = `${size}px`
+        },
+    },
+    watch: {
+        isTurn() {
             this.sizeCard()
         },
-        beforeUnmount() {
-            window.removeEventListener('resize', this.sizeCard)
-        },
-    }
+    },
+    mounted() {
+        window.addEventListener('resize', this.sizeCard)
+        this.sizeCard()
+    },
+    beforeUnmount() {
+        window.removeEventListener('resize', this.sizeCard)
+    },
+}
 </script>
 
 <style lang="scss" scoped>
-    @import '../variables.scss';
+@import '../variables.scss';
 
-    .playing-card {
-        position:    relative;
-        width:       100%;
-        height: 100%;
+.playing-card {
+    position: relative;
+    width:    100%;
+    aspect-ratio: 2.5 / 3.5;
+}
+
+div.big-card {
+    background-color: white;
+    border-radius:    $border-radius;
+    border:           1px solid #eee;
+    position:         absolute;
+    top:              0;
+    right:            0;
+    bottom:           0;
+    left:             0;
+
+    &.hearts, &.diamonds {
+        color: $red;
+
+        :deep(svg) {
+            fill: $red;
+
+            &.wild {
+                fill: $yellow;
+            }
+        }
     }
 
-    div.big-card {
-        background-color: white;
-        border-radius:    $border-radius;
-        border:           1px solid #eee;
-        position:         absolute;
-        top:              0;
-        right:            0;
-        bottom:           0;
-        left:             0;
+    &.stars {
+        color: $light-green;
 
-        &.hearts, &.diamonds {
-            color: $red;
+        :deep(svg) {
+            fill: $light-green;
 
-            :deep(svg) {
-                fill: $red;
-
-                &.wild {
-                    fill: $yellow;
-                }
+            &.wild {
+                fill: $yellow;
             }
-        }
-
-        &.stars {
-            color: $light-green;
-
-            :deep(svg) {
-                fill: $light-green;
-
-                &.wild {
-                    fill: $yellow;
-                }
-            }
-        }
-
-        .corner {
-            position:       absolute;
-            display:        flex;
-            flex-direction: column;
-            top:            2px;
-            left:           2px;
-            width:          min-content;
-            align-items:    center;
-
-            span.suit {
-                line-height: 40%;
-                height:      100%;
-                width:       100%;
-            }
-
-            span.rank {
-                font-size:   32px;
-                line-height: 28px;
-            }
-        }
-
-        .wild {
-            position: absolute;
-            left: 0;
-            bottom: 0;
-            width: 30%;
-            fill: $yellow;
-        }
-
-        .center {
-            position:  absolute;
-            top:       45%;
-            right:     45%;
-            transform: translateX(50%);
-            width:     60%;
         }
     }
+
+    .corner {
+        position:       absolute;
+        display:        flex;
+        flex-direction: column;
+        top:            2px;
+        left:           2px;
+        width:          min-content;
+        align-items:    center;
+
+        span.suit {
+            line-height: 40%;
+            height:      100%;
+            width:       100%;
+        }
+
+        span.rank {
+            font-size:   32px;
+            line-height: 28px;
+        }
+    }
+
+    .wild {
+        position: absolute;
+        left:     0;
+        bottom:   0;
+        width:    30%;
+        fill:     $yellow;
+    }
+
+    .center {
+        position:  absolute;
+        top:       45%;
+        right:     45%;
+        transform: translateX(50%);
+        width:     60%;
+    }
+}
 </style>
--- src/components/games/passthepoop/PassThePoopParticipant.vue
+++ src/components/games/passthepoop/PassThePoopParticipant.vue
@@ -2,10 +2,12 @@
     <div :class="{ 'ptp-participant': true, 'is-connected': isConnected }">
         <strong class="name">{{ playerData.player.displayName }}</strong>
 
-        <transition-group name="lives" class="lives">
-            <mdi-icon :icon="mdiPokerChip" v-for="i in participant.lives" :key="i"/>
-            <span class="placeholder" v-if="participant.lives === 0" :key="0"></span>
-        </transition-group>
+        <div class="lives">
+            <transition-group name="lives" class="lives">
+                <mdi-icon :icon="mdiPokerChip" v-for="i in participant.lives" :key="i"/>
+                <span class="placeholder" v-if="participant.lives === 0" :key="0"></span>
+            </transition-group>
+        </div>
 
         <div class="ptp-card-container">
             <playing-card-container class="ptp-card" :card="card" :hide-card="hideCard"/>
@@ -22,175 +24,175 @@
 </template>
 
 <script>
-    import MdiIcon from "../../MdiIcon.vue"
-    import {mdiCardsPlayingOutline, mdiPokerChip, mdiTimerSand, mdiCardsOutline} from '@mdi/js'
-    import PlayingCardContainer from "../../PlayingCardContainer.vue"
+import MdiIcon from "../../MdiIcon.vue"
+import {mdiCardsPlayingOutline, mdiPokerChip, mdiTimerSand, mdiCardsOutline} from '@mdi/js'
+import PlayingCardContainer from "../../PlayingCardContainer.vue"
 
-    export default {
-        name: "PassThePoopParticipant",
-        components: {PlayingCardContainer, MdiIcon},
-        props: {
-            participant: {
-                type: Object,
-                required: true,
-            },
-            playerData: {
-                type: Object,
-                required: true,
-            },
+export default {
+    name: "PassThePoopParticipant",
+    components: {PlayingCardContainer, MdiIcon},
+    props: {
+        participant: {
+            type: Object,
+            required: true,
         },
-        data() {
-            return {
-                mdiCardsPlayingOutline,
-                mdiPokerChip,
-                mdiCardsOutline,
-                mdiTimerSand,
-                hideCard: false,
-                cardTransition: 'flip-card',
-            }
+        playerData: {
+            type: Object,
+            required: true,
         },
-        computed: {
-            isConnected() {
-                return this.playerData.isConnected
-            },
-            card() {
-                return this.participant.card
-            },
-            isPlayerTurn() {
-                return this.$store.getters['passThePoop/isPlayerTurn'](this.participant.playerId)
-            },
-            lastGameAction() {
-                return this.$store.getters['passThePoop/gameData'].gameState.lastGameAction
-            },
-            goingToDeck() {
-                return this.lastGameAction && this.lastGameAction.gameAction.name === 'Go to Deck'
-            },
-            didTradeCard() {
-                const lastGameAction = this.lastGameAction
-                return lastGameAction &&
-                    lastGameAction.gameAction.name === 'Trade' &&
-                    lastGameAction.playerId === this.participant.playerId
-            },
+    },
+    data() {
+        return {
+            mdiCardsPlayingOutline,
+            mdiPokerChip,
+            mdiCardsOutline,
+            mdiTimerSand,
+            hideCard: false,
+            cardTransition: 'flip-card',
+        }
+    },
+    computed: {
+        isConnected() {
+            return this.playerData.isConnected
         },
-        watch: {
-            'participant.isCardDead': {
-                handler(isCardDead) {
-                    if (isCardDead) {
-                        setTimeout(() => {
-                            console.log("HIDE")
-                            this.hideCard = true
-                        }, 2000)
-                    } else {
-                        this.hideCard = false
-                    }
-                },
-                immediate: true,
-            },
-            didTradeCard: {
-                handler(didTradeCard) {
-                    this.hideCard = didTradeCard
-                },
-                immediate: true,
-            },
+        card() {
+            return this.participant.card
         },
-    }
+        isPlayerTurn() {
+            return this.$store.getters['passThePoop/isPlayerTurn'](this.participant.playerId)
+        },
+        lastGameAction() {
+            return this.$store.getters['passThePoop/gameData'].gameState.lastGameAction
+        },
+        goingToDeck() {
+            return this.lastGameAction && this.lastGameAction.gameAction.name === 'Go to Deck'
+        },
+        didTradeCard() {
+            const lastGameAction = this.lastGameAction
+            return lastGameAction &&
+                lastGameAction.gameAction.name === 'Trade' &&
+                lastGameAction.playerId === this.participant.playerId
+        },
+    },
+    watch: {
+        'participant.isCardDead': {
+            handler(isCardDead) {
+                if (isCardDead) {
+                    setTimeout(() => {
+                        console.log("HIDE")
+                        this.hideCard = true
+                    }, 2000)
+                } else {
+                    this.hideCard = false
+                }
+            },
+            immediate: true,
+        },
+        didTradeCard: {
+            handler(didTradeCard) {
+                this.hideCard = didTradeCard
+            },
+            immediate: true,
+        },
+    },
+}
 </script>
 
 <style lang="scss" scoped>
-    @import '../../../variables';
+@import '../../../variables';
 
-    .ptp-participant {
-        display:        flex;
-        flex-direction: column;
-        max-width:      100px;
-        align-items:    center;
+.ptp-participant {
+    display:        flex;
+    flex-direction: column;
+    max-width:      100px;
+    align-items:    center;
 
+    strong.name {
+        overflow: hidden;
+        display:  block;
+
+        @media(max-width: 500px) {
+            font-size: 10px;
+        }
+    }
+
+    &:not(.is-connected) {
         strong.name {
-            overflow: hidden;
-            display:  block;
-
-            @media(max-width: 500px) {
-                font-size: 10px;
-            }
-        }
-
-        &:not(.is-connected) {
-            strong.name {
-                font-weight: normal;
-                font-style:  italic;
-                color:       $text-color-light;
-            }
-        }
-
-        .lives {
-            display:               grid;
-            grid-template-columns: repeat(3, 1fr);
-            grid-gap:              $spacing-small;
-            margin:                $spacing-small 0;
-            width:                 100%;
-            align-items:           center;
-
-            svg {
-                width: 100%;
-            }
-
-            span.placeholder {
-                width:       100%;
-                padding-top: 100%;
-                height:      0;
-            }
-        }
-
-        span.is-turn {
-            display:    inline-block;
-            width:      25px;
-            height:     25px;
-            margin-top: $spacing-small;
-        }
-
-        div.ptp-card-container {
-            position: relative;
-            width:    100%;
-
-            .block-chip {
-                position:         absolute;
-                top:              5px;
-                right:            5px;
-                width:            19px;
-                height:           19px;
-                background-color: $red;
-                border-radius:    50%;
-                border:           2px dotted white;
-                box-shadow:       2px 1px rgba(black, 0.3);
-
-                fill:             $red;
-                outline:          white 3px;
-            }
+            font-weight: normal;
+            font-style:  italic;
+            color:       $text-color-light;
         }
     }
 
-    .lives-leave-active {
-        animation:       lose-life 500ms;
-        animation-delay: 1000ms;
-    }
+    .lives {
+        display:               grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-gap:              $spacing-small;
+        margin:                $spacing-small 0;
+        width:                 100%;
+        align-items:           center;
 
-    @keyframes lose-life {
-        from {
-            opacity:   1;
-            transform: none;
+        svg {
+            width: 100%;
         }
-        to {
-            opacity:   0;
-            transform: translateY(-100%);
+
+        span.placeholder {
+            width:       100%;
+            padding-top: 100%;
+            height:      0;
         }
     }
 
-    .block-leave-active {
-        transition: 1000ms all;
+    span.is-turn {
+        display:    inline-block;
+        width:      25px;
+        height:     25px;
+        margin-top: $spacing-small;
     }
 
-    .block-leave-to {
-        transform: translateY(-200%);
+    div.ptp-card-container {
+        position: relative;
+        width:    100%;
+
+        .block-chip {
+            position:         absolute;
+            top:              5px;
+            right:            5px;
+            width:            19px;
+            height:           19px;
+            background-color: $red;
+            border-radius:    50%;
+            border:           2px dotted white;
+            box-shadow:       2px 1px rgba(black, 0.3);
+
+            fill:             $red;
+            outline:          white 3px;
+        }
+    }
+}
+
+.lives-leave-active {
+    animation:       lose-life 500ms;
+    animation-delay: 1000ms;
+}
+
+@keyframes lose-life {
+    from {
+        opacity:   1;
+        transform: none;
+    }
+    to {
         opacity:   0;
+        transform: translateY(-100%);
     }
+}
+
+.block-leave-active {
+    transition: 1000ms all;
+}
+
+.block-leave-to {
+    transform: translateY(-200%);
+    opacity:   0;
+}
 </style>
