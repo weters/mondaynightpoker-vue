8499f54c622b2688b6619f82f63b7fe558434d36
Tom Peters <weters@me.com>
2026-03-01 22:52:44 -0500

    Fix sub-pixel border rendering on card back

--- src/components/PlayingCardBack.vue
+++ src/components/PlayingCardBack.vue
@@ -19,7 +19,7 @@ div.playing-card-back {
     border-radius: $border-radius-card;
     position:      relative;
     overflow:      hidden;
-    border:        1px solid #d0c8b8;
+    border:        0.99px solid #d0c8b8;
     box-shadow:    inset 0 1px 3px rgba(0, 0, 0, 0.06),
                    inset 0 -1px 2px rgba(255, 255, 255, 0.8),
                    0 2px 6px rgba(0, 0, 0, 0.1);
